e8d6875491cd2682fe54776799d98d38f559123b
Matias <matiasezeqsoto@gmail.com>
2024-05-01 09:42:47 -0300

    commit desde Desktop soto

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -5,16 +5,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.profile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var lat: Double = 0.0
+    private var long: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+
+        updateUI()
+
+        binding.tvLocation.setOnClickListener {
+            binding.tvLocation.text = "Lat: $lat, Long: $long"
         }
+    }
+
+    private fun updateUI(name: String = "Proyecto Kotlin PW", email: String = "pwkotlinsoft@gmail.com",
+                         website: String = "pwsoft.com", phone: String = "+54 231321321") {
+
+        binding.tvName.text = name
+        binding.tvEmail.text = email
+        binding.tvWebsite.text = website
+        binding.tvPhone.text = phone
+        lat = -38.940576
+        long = -68.068939
     }
 }

096429105e190281f1fd5956526e584c62a127b6
Matias <matiasezeqsoto@gmail.com>
2024-05-05 18:34:26 -0300

    Elimina y restaura los datos desde Settings

--- app/src/main/java/com/example/profile/SettingsFragment.kt
+++ app/src/main/java/com/example/profile/SettingsFragment.kt
@@ -1,7 +1,10 @@
 package com.example.profile
 
 import android.os.Bundle
+import androidx.core.content.edit
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
 //funciona como actividad donde se vincula una vista NO PUEDE EXISTIR POR SI SOLO SI NO QUE TIENE
@@ -9,5 +12,62 @@ import androidx.preference.PreferenceFragmentCompat
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey) //VINCULADO A LA VISTA
+
+        /*        ESTE BLOQUE DE CODIGO LE DICE AL BOTON DE "ELIMINAR DATOS DEL USUARIO" QUE LOS RESTABLEZCA
+        Y AL SALIR DE LA APP Y VOLVER A ENTRAR LOS DATOS ALTERADOS SON NUEVAMENTE LOS PREDETERMINADOS*/
+        val deleteUserDataPreferenceFragment =
+            findPreference<Preference>(getString(R.string.preference_key_delete_data))
+        deleteUserDataPreferenceFragment?.setOnPreferenceClickListener {
+            val sharePreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+            sharePreferences.edit {
+                putString(getString(R.string.key_img), null)
+                putString(getString(R.string.key_name), null)
+                putString(getString(R.string.key_email), null)
+                putString(getString(R.string.key_website), null)
+                putString(getString(R.string.key_phone), null)
+                putString(getString(R.string.key_latitud), null)
+                putString(getString(R.string.key_longitud), null)
+                apply()
+            }
+            true
+        }
+
+        /*ELIMINA POR COMPLETO TODAS LAS COFIGURACIONES Y DATOS EDITADOS */
+        val restoreAllPreference =
+            findPreference<Preference>(getString(R.string.preference_key_restore_data))
+        restoreAllPreference?.setOnPreferenceClickListener {
+            val sharePreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+            sharePreferences.edit().clear()
+                .apply() /*ESTE METODO .edit().clear().apply() LIMPIA POR COMPLETO LAS PREFERENCIASDATOS Y CONFIGURACIONES*/
+            true
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
